4f416aa74e462222c0e32174f9627ff27a2d43df
Rezwan Atcha <rez_a@outlook.com>
2023-05-01 12:28:52 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [currentLights1, setCurrentLights1] = useState(1);
+  const [TLights1, setTLights1] = useState(1);
   const [lightSwitch, setLightSwitch] = useState(false);
 
-  const currentLights1Handler = () => {
-    for (let i = 0; i < 3; i++) {
-      setCurrentLights1(i);
+  const TLights1Handler = () => {
+    let i = 0;
+    while (i < 3) {
+      setTLights1(i);
+      i++;
     }
-    setCurrentLights1(currentLights1 + 1);
+    setTLights1(TLights1 + 1);
     lightRestartHandler();
   };
 
-  // function toggleLightSwitcher(e) {
-  //   setInterval(toggleLight, 1000);
-  // }
-
   const lightSwitchHandler = () => {
-    setLightSwitch((lightSwitch) => !lightSwitch);
-    // toggleLightSwitcher();
-    setInterval(toggleLight, 1000);
-  };
-
-  const toggleLight = () => {
-    currentLights1Handler();
-    // toggleLightSwitcher();
+    setInterval(TLights1Handler, 1000);
   };
 
   function lightRestartHandler() {
-    if (currentLights1 === 3) {
-      setCurrentLights1(1);
+    if (TLights1 === 3) {
+      setTLights1(1);
     }
   }
 
@@ -63,22 +54,20 @@ function App() {
     width: '40px',
     height: '40px',
     borderRadius: '50px',
-    backgroundColor: currentLights1 === 3 ? 'red' : 'rgb(113, 37, 37)',
+    backgroundColor: TLights1 === 3 ? 'red' : 'rgb(113, 37, 37)',
   };
 
   const yellowLight = {
     width: '40px',
     height: '40px',
     borderRadius: '50px',
-    backgroundColor:
-      currentLights1 === 2 ? 'rgb(255, 255, 0)' : 'rgb(104, 104, 14)',
+    backgroundColor: TLights1 === 2 ? 'rgb(255, 255, 0)' : 'rgb(104, 104, 14)',
   };
   const greenLight = {
     width: '40px',
     height: '40px',
     borderRadius: '50px',
-    backgroundColor:
-      currentLights1 === 1 ? 'rgb(20, 197, 20)' : 'rgb(6, 67, 6)',
+    backgroundColor: TLights1 === 1 ? 'rgb(20, 197, 20)' : 'rgb(6, 67, 6)',
   };
 
   return (
