fadc3f8891812c31b0122204b6986b29cf640712
Rezwan Atcha <rez_a@outlook.com>
2023-06-22 13:35:07 -0400

    The light can now stop upon clicking the stop button.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
+let lightSwitchInterval;
 function App() {
-  const [TLights1, setTLights1] = useState(1);
-  // const [lightSwitch, setLightSwitch] = useState(false);
-
-  const TLights1Handler = () => {
-    let i = 0;
-    while (i < 3) {
-      setTLights1(i);
-      i++;
-    }
-    setTLights1(TLights1 + 1);
-    lightRestartHandler();
-  };
-
-  // const lightSwitchHandler = () => {
-  //   setInterval(TLights1Handler, 1000);
-  // };
-  const clearTimer = () => {
-    clearTimeout(TLights1Handler);
-  };
+  const [TLights1, setTLights1] = useState(0);
 
   useEffect(() => {
-    setTimeout(TLights1Handler, 1000);
-  });
+    lightSwitchInterval = setInterval(TLights1Handler, 1000);
 
-  function lightRestartHandler() {
-    if (TLights1 === 3) {
-      setTLights1(1);
-    }
-  }
+    return () => clearInterval(lightSwitchInterval);
+  }, []);
+
+  const TLights1Handler = () => {
+    setTLights1((currentState) => (currentState + 1) % 3);
+  };
+
+  const stopInterval = () => {
+    clearInterval(lightSwitchInterval);
+  };
 
   const trafficLight1 = {
     display: 'flex',
@@ -61,25 +48,25 @@ function App() {
     width: '40px',
     height: '40px',
     borderRadius: '50px',
-    backgroundColor: TLights1 === 3 ? 'red' : 'rgb(113, 37, 37)',
+    backgroundColor: TLights1 === 2 ? 'red' : 'rgb(113, 37, 37)',
   };
 
   const yellowLight = {
     width: '40px',
     height: '40px',
     borderRadius: '50px',
-    backgroundColor: TLights1 === 2 ? 'rgb(255, 255, 0)' : 'rgb(104, 104, 14)',
+    backgroundColor: TLights1 === 1 ? 'rgb(255, 255, 0)' : 'rgb(104, 104, 14)',
   };
   const greenLight = {
     width: '40px',
     height: '40px',
     borderRadius: '50px',
-    backgroundColor: TLights1 === 1 ? 'rgb(20, 197, 20)' : 'rgb(6, 67, 6)',
+    backgroundColor: TLights1 === 0 ? 'rgb(20, 197, 20)' : 'rgb(6, 67, 6)',
   };
 
   return (
     <>
-      <button onClick={clearTimer}>Stop</button>
+      <button onClick={stopInterval}>Stop</button>
       <div style={trafficLight1}>
         <object style={redLight}></object>
         <object style={yellowLight}></object>
@@ -93,5 +80,99 @@ function App() {
     </>
   );
 }
+
+// function App() {
+//   const [TLights1, setTLights1] = useState(1);
+//   // const [lightSwitch, setLightSwitch] = useState(false);
+
+//   const TLights1Handler = () => {
+//     let i = 0;
+//     while (i < 3) {
+//       setTLights1(i);
+//       i++;
+//     }
+//     setTLights1(TLights1 + 1);
+//     lightRestartHandler();
+//   };
+
+//   // const lightSwitchHandler = () => {
+//   //   setInterval(TLights1Handler, 1000);
+//   // };
+//   const clearTimer = () => {
+//     clearTimeout(TLights1Handler);
+//   };
+
+//   useEffect(() => {
+//     setTimeout(TLights1Handler, 1000);
+//   });
+
+//   function lightRestartHandler() {
+//     if (TLights1 === 3) {
+//       setTLights1(1);
+//     }
+//   }
+
+//   const trafficLight1 = {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     justifyContent: 'space-around',
+//     alignItems: 'center',
+//     position: 'relative',
+//     top: '60px',
+//     left: '300px',
+//     backgroundColor: '#333',
+//     width: '60px',
+//     height: '200px',
+//   };
+
+//   const trafficLight2 = {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     justifyContent: 'space-around',
+//     alignItems: 'center',
+//     position: 'relative',
+//     top: '300px',
+//     left: '300px',
+//     backgroundColor: '#333',
+//     width: '60px',
+//     height: '200px',
+//   };
+
+//   const redLight = {
+//     width: '40px',
+//     height: '40px',
+//     borderRadius: '50px',
+//     backgroundColor: TLights1 === 3 ? 'red' : 'rgb(113, 37, 37)',
+//   };
+
+//   const yellowLight = {
+//     width: '40px',
+//     height: '40px',
+//     borderRadius: '50px',
+//     backgroundColor: TLights1 === 2 ? 'rgb(255, 255, 0)' : 'rgb(104, 104, 14)',
+//   };
+//   const greenLight = {
+//     width: '40px',
+//     height: '40px',
+//     borderRadius: '50px',
+//     backgroundColor: TLights1 === 1 ? 'rgb(20, 197, 20)' : 'rgb(6, 67, 6)',
+//   };
+
+//   return (
+//     <>
+//       <button onClick={clearTimer}>Stop</button>
+//       <div style={trafficLight1}>
+//         <object style={redLight}></object>
+//         <object style={yellowLight}></object>
+//         <object style={greenLight}></object>
+//       </div>
+//       <div style={trafficLight2}>
+//         <object style={redLight}></object>
+//         <object style={yellowLight}></object>
+//         <object style={greenLight}></object>
+//       </div>
+//     </>
+//   );
+// }
 
 export default App;
